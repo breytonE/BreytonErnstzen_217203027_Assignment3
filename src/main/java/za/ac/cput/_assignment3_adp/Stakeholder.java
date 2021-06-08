7cd4f6e14bd50461a13dfe3d351a91b7b29e8da2
breySean <breytonernstzen@gmail.com>
2021-06-09 01:11:52 +0200

    test method removed

--- src/main/java/za/ac/cput/_assignment3_adp/Stakeholder.java
+++ src/main/java/za/ac/cput/_assignment3_adp/Stakeholder.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package za.ac.cput._assignment3_adp;
 
 import java.io.Serializable;
 
 /**
  *
- * @author Breyton Ernstzen
+ * @author Breyton Ernstzen (217203027)
  */
 public class Stakeholder implements Serializable{
      private String stHolderId;
